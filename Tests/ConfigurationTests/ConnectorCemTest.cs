594a62ca6639957e02b789fa5ee45cc282c41179
FedericoArnaudo <federicoarnaudo.97@gmail.com>
2025-01-10 14:47:46 -0300

    Prepare tests for Despachos

--- Tests/ConfigurationTests/ConnectorCemTest.cs
+++ Tests/ConfigurationTests/ConnectorCemTest.cs
@@ -13,11 +13,17 @@ namespace ConfigurationTests
     [TestClass]
     public class ConnectorCemTest
     {
+        private Mock<IConnections> connections;
+        private ConnectorCem connectorCem;
         private Data data;
 
         [TestInitialize]
         public void TestInitialize()
         {
+            connections = new Mock<IConnections>();
+
+            connectorCem = new ConnectorCem(connections.Object);
+
             data = new Data
             {
                 IP = "10.773.856",
@@ -26,6 +32,8 @@ namespace ConfigurationTests
             };
 
             _ = Configuration.SaveConfiguration(data);
+
+            _ = connections.Setup(a => a.GetConfiguration()).Returns(data);
         }
 
         [TestCleanup]
@@ -42,15 +50,11 @@ namespace ConfigurationTests
         public void PoleoEnLinea_ReturnTrue()
         {
             // Arange
-            Mock<IConnections> connections = new Mock<IConnections>();
-
             byte[] command = new byte[] { 0x00 };
 
             _ = connections.Setup(a => a.EnviarComando(command)).Returns(new byte[] { 0x00 });
 
             _ = connections.Setup(a => a.GetConfiguration()).Returns(data);
-
-            ConnectorCem connectorCem = new ConnectorCem(connections.Object);
 
             //Act
             bool actual = connectorCem.PoleoEnLinea(command);
@@ -63,15 +67,9 @@ namespace ConfigurationTests
         public void PoleoEnLinea_ReturnFalse_InBuffer()
         {
             // Arange
-            Mock<IConnections> connections = new Mock<IConnections>();
-
             byte[] command = new byte[] { 0x00 };
 
             _ = connections.Setup(a => a.EnviarComando(command)).Returns(new byte[] { 0x01 });
-
-            _ = connections.Setup(a => a.GetConfiguration()).Returns(data);
-
-            ConnectorCem connectorCem = new ConnectorCem(connections.Object);
 
             //Act
             bool actual = connectorCem.PoleoEnLinea(command);
@@ -84,15 +82,9 @@ namespace ConfigurationTests
         public void PoleoEnLinea_ThrowException()
         {
             // Arange
-            Mock<IConnections> connections = new Mock<IConnections>();
-
             byte[] command = new byte[] { 0x00 };
 
             _ = connections.Setup(a => a.EnviarComando(command)).Returns(new byte[] { });
-
-            _ = connections.Setup(a => a.GetConfiguration()).Returns(data);
-
-            ConnectorCem connectorCem = new ConnectorCem(connections.Object);
 
             // Act
             Exception ex = Assert.ThrowsException<Exception>(() => connectorCem.PoleoEnLinea(command));
@@ -104,17 +96,11 @@ namespace ConfigurationTests
         public void ComandoConfiguracionDeLaEstacion_ReturnNotNull()
         {
             // Arange
-            Mock<IConnections> connections = new Mock<IConnections>();
-
-            ConnectorCem connectorCem = new ConnectorCem(connections.Object);
-
             byte[] reply = connectorCem.ReadAnswer("ConfiguracionDeLaEstacion");
 
             byte[] command = new byte[] { 0x65 };
 
             _ = connections.Setup(a => a.EnviarComando(command)).Returns(reply);
-
-            _ = connections.Setup(a => a.GetConfiguration()).Returns(data);
 
             //Act
             Station actual = connectorCem.ComandoConfiguracionDeLaEstacion(command);
@@ -127,15 +113,9 @@ namespace ConfigurationTests
         public void ComandoConfiguracionDeLaEstacion_ThrowException()
         {
             // Arange
-            Mock<IConnections> connections = new Mock<IConnections>();
-
-            ConnectorCem connectorCem = new ConnectorCem(connections.Object);
-
             byte[] command = new byte[] { 0x65 };
 
             _ = connections.Setup(a => a.EnviarComando(command)).Returns(new byte[] { });
-
-            _ = connections.Setup(a => a.GetConfiguration()).Returns(data);
 
             //Act
             Exception ex = Assert.ThrowsException<Exception>(() => connectorCem.ComandoConfiguracionDeLaEstacion(command));
@@ -147,15 +127,9 @@ namespace ConfigurationTests
         public void ComandoConfiguracionDeLaEstacion_ThrowException_NotConfirmation()
         {
             // Arange
-            Mock<IConnections> connections = new Mock<IConnections>();
-
-            ConnectorCem connectorCem = new ConnectorCem(connections.Object);
-
             byte[] command = new byte[] { 0x65 };
 
             _ = connections.Setup(a => a.EnviarComando(command)).Returns(new byte[] { 0x01 });
-
-            _ = connections.Setup(a => a.GetConfiguration()).Returns(data);
 
             string expected = "Error al obtener la configuración de la estación. Excepción: No se recibió mensaje de confirmación al solicitar la configuración de la estación.";
 
@@ -170,17 +144,11 @@ namespace ConfigurationTests
         public void ComandoStockDeTanques_ReturnNotNull()
         {
             // Arange
-            Mock<IConnections> connections = new Mock<IConnections>();
-
-            ConnectorCem connectorCem = new ConnectorCem(connections.Object);
-
             byte[] reply = connectorCem.ReadAnswer("StockDeTanques");
 
             byte[] command = new byte[] { 0x68 };
 
             _ = connections.Setup(a => a.EnviarComando(command)).Returns(reply);
-
-            _ = connections.Setup(a => a.GetConfiguration()).Returns(data);
 
             //Act
             List<Tanque> actual = connectorCem.ComandoStockDeTanques(command);
@@ -193,23 +161,47 @@ namespace ConfigurationTests
         public void ComandoStockDeTanques_ReturnNull_NotConfirmation()
         {
             // Arange
-            Mock<IConnections> connections = new Mock<IConnections>();
-
-            ConnectorCem connectorCem = new ConnectorCem(connections.Object);
-
             byte[] reply = new byte[] { 0x01 };
 
             byte[] command = new byte[] { 0x68 };
 
             _ = connections.Setup(a => a.EnviarComando(command)).Returns(reply);
 
-            _ = connections.Setup(a => a.GetConfiguration()).Returns(data);
-
             //Act
             List<Tanque> actual = connectorCem.ComandoStockDeTanques(command);
 
             // Assert
             Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void GrabarDespachos_ReturnNotNull()
+        {
+            // Arange
+
+            //Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void GrabarDespachos_ReturnNotNull_WithoutSales()
+        {
+            // Arange
+
+            //Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void GrabarDespachos_ReturnNull_NotConfirmation()
+        {
+            // Arange
+
+            //Act
+
+            // Assert
         }
     }
 }

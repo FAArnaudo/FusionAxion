3207a78430c2529a005d11f5d4e0d9f21951a5f7
FedeArnaudo <federicoarnaudo.97@gmail.com>
2025-03-09 15:15:38 -0300

    Class Button Custom

---
+++ FusionAxion/BotonPersonalizado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FusionAxion
+{
+    // Clase que representa un botón personalizado
+    public class BotonPersonalizado : Button
+    {
+        public int Id { get; private set; }
+
+        public BotonPersonalizado(string texto, int id)
+        {
+            Id = id;
+            Content = texto;
+            Height = 80;
+            Width = 60;
+            Margin = new Thickness(5);
+            Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#ADD8E6");
+            Click += Boton_Click;
+        }
+
+        private void Boton_Click(object sender, RoutedEventArgs e)
+        {
+            _ = MessageBox.Show($"Se presionó el botón {Content}");
+        }
+    }
+}

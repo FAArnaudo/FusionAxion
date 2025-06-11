dc3c9bc5b7166f3961b6f8fb089e9c07c27c9c61
Federico Andres Arnaudo <federicoarnaudo.97@gmail.com>
2025-06-11 12:06:02 -0300

    update wd

--- CDS/WatchDog.cs
+++ CDS/WatchDog.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace CDS
 {
